0b902bc6e1f1940a3e4f887a9c7c37c4060bb6d8
sargbjornson <sarg.auxiliar@gmail.com>
2018-08-30 18:34:01 +0200

    v3.1

---
+++ Source/NewHatcher/NewHatcher/HediffComp_Milker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+
+namespace NewHatcher
+{
+    public class HediffComp_Milker : HediffComp
+    {
+
+        private int HatchingTicker = 0;
+
+        public HediffCompProperties_Milker Props
+        {
+            get
+            {
+                return (HediffCompProperties_Milker)this.props;
+            }
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            Hatch();
+        }
+
+        public void Hatch()
+        {
+            if (HatchingTicker < (this.Props.hatcherDaystoHatch*60000)) {
+                HatchingTicker += 1;
+            } else
+            {
+                if ((this.parent.pawn.Map != null)&&(this.parent.pawn.Faction == Faction.OfPlayer)) { 
+                    GenSpawn.Spawn(ThingDef.Named("Milk"), this.parent.pawn.Position, this.parent.pawn.Map);
+                }
+                HatchingTicker = 0;
+
+            }
+
+
+            //this.parent.Destroy(DestroyMode.Vanish);
+        }
+
+
+    }
+}
